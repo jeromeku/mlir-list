085d0584b77973d211a89435e19ed7fba1af35d4
jeromeku <jerome.ku@gmail.com>
2025-10-19 22:45:34 +0000

    add build script

--- build.sh
+++ build.sh
@@ -1,27 +1,32 @@
 #!/bin/bash
+set -euo pipefail
+LLVM_ROOT=$(dirname `realpath -L .`)
+echo $LLVM_ROOT
+LLVM_BUILD_DIR=${LLVM_ROOT}/build
 
-mkdir -p build
-cd build
+CC=/usr/bin/clang-22
+CXX=/usr/bin/clang++-22
+BUILD_DIR="build"
+BUILD_TYPE="Debug"
 
-# increase max number of open files for linking
-ulimit -n 2048
+cmake -S . -B $BUILD_DIR -G "Ninja"               \
+    -DLLVM_DIR=${LLVM_BUILD_DIR}/lib/cmake/llvm   \
+    -DMLIR_DIR=${LLVM_BUILD_DIR}/lib/cmake/mlir   \
+    -DMLIR_ENABLE_BINDINGS_PYTHON=ON \
+    -DPython3_FIND_VIRTUALENV=FIRST \
+    -DLLVM_ENABLE_LLD=On \
+    -DLLVM_BUILD_LLVM_DYLIB=ON \
+    -DLLVM_LINK_LLVM_DYLIB=ON \
+    -Wno-dev \
+    -DCMAKE_C_COMPILER=$CC                        \
+    -DCMAKE_CXX_COMPILER=$CXX                     \
+    -DCMAKE_EXPORT_COMPILE_COMMANDS=ON            \
+    -DCMAKE_VERBOSE_MAKEFILE=ON            \
+    -DCMAKE_BUILD_TYPE=${BUILD_TYPE} \
+    --debug-output 2>&1 | tee _cmake.config.log
 
-LLVM_PREFIX=$(python3 -c 'import mlir;print(mlir.__path__[0])')
+#ninja -C build -d explain -v check-listproject 2>&1 | tee _cmake.build.log
 
-cmake -G Ninja \
-  -DMLIR_DIR=$LLVM_PREFIX/lib/cmake/mlir \
-  -DLLVM_EXTERNAL_LIT=$(which lit) \
-  -DCMAKE_C_COMPILER=/usr/bin/clang \
-  -DCMAKE_CXX_COMPILER=/usr/bin/clang++ \
-  -DMLIR_ENABLE_BINDINGS_PYTHON=ON \
-  -DPython_FIND_VIRTUALENV=FIRST \
-  -DPython3_FIND_VIRTUALENV=FIRST \
-  -DLLVM_ENABLE_LLD=On \
-  -DLLVM_BUILD_LLVM_DYLIB=ON \
-  -DLLVM_LINK_LLVM_DYLIB=ON \
-  -DCMAKE_BUILD_TYPE=Debug \
-  -Wno-dev \
-  ..
-
-cmake --build . --target check-listproject
-
+    # -DCMAKE_EXE_LINKER_FLAGS="-fuse-ld=lld -Wl,--ld-path=$(command -v ld.lld-22)" \
+    # -DCMAKE_SHARED_LINKER_FLAGS="-fuse-ld=lld -Wl,--ld-path=$(command -v ld.lld-22)" \
+    # -DCMAKE_MODULE_LINKER_FLAGS="-fuse-ld=lld -Wl,--ld-path=$(command -v ld.lld-22)"
